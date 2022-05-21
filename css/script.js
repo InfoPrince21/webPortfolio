07ade8fe95542e70cf50943d7dcabc114b4e5350
Prince <princeitschool@gmail.com>
2022-05-21 03:23:56 -0500

    animations

---
+++ css/script.js
@@ -0,0 +1,68 @@
+
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll(".slide-in");
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -75px 0px"
+};
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+},
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
+
+// $(function() {
+//     $(window).scroll(function() {
+//         let scrollpos = ($(document).height() - $(window).height() - $(window).scrollTop() ) ;
+    
+//         $('.bg').css({
+//             'opacity': + (scrollpos/25) + '%'
+//         })
+//         console.log(scrollpos/25)
+//     })
+
+//  });
+
+
+ $(function() {
+    $(window).scroll(function() {
+        let scrollpos = ($(window).scrollTop());
+        $('.bg').css({
+            'opacity': + (scrollpos) + '%'
+        })
+        console.log(scrollpos)
+    })
+
+ });
+
+
+ 
+// $(function() {
+    
+//     let $animate = $(".animate");
+//     let $win = $(window);
+
+//     $win.on('scroll', function () {
+//         let top = $win.scrollTop() / 5;
+//         $animate.css('transform', 'skewX(' + top + 'deg) ');
+//     })
+
+//  });
